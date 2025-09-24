4b829609c7216cff880d9cc11de1c0e9f69d53dd
rizars <riza@realsecure.ae>
2025-09-25 00:08:08 +0400

    updated project structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hello World this is me making a change
+    <div
+      className="min-h-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: "url('/assets/backdrop.svg')" }}
+    >
+
+      <h1 className="text-white text-4xl p-10">Hello World</h1>
+    
     </div>
   );
 }
